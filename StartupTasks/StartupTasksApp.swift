aae133bd94ed76b201e1cecfdcc9e20076dd6011
Gergo Fazekas <gergo.fazekas@garmin.com>
2024-05-25 22:32:08 +0300

    Initial Commit

---
+++ StartupTasks/StartupTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartupTasksApp.swift
+//  StartupTasks
+//
+//  Created by Fazekas, Gergo on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct StartupTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
